ddd5dd37281497514e2f901fe5baa31e7e75e80f
Julio Ramirez <mesiecuac@gmail.com>
2023-02-20 13:36:07 -0600

    agregando grafica

--- grafica.py
+++ grafica.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 x = np.arange(0,100,0.1)
 y = x*np.cos(x)
 
+t = np.arange(0.,10.,1)
+
+plt.plot(t,t, 'r--', t,t**2,'bs')
 plt.plot(x,y)
 plt.xlabel('x')
 plt.ylabel('y')
