bdd60d5629c5fde0301e3f5609f27a97b64121ea
Julio Ramirez <mesiecuac@gmail.com>
2023-06-28 23:19:26 -0600

    agrgando valores a java

--- grafica.py
+++
@@ -1,9 +0,0 @@
-import numpy as np 
-import matplotlib.pyplot as plt
-
-x = np.linspace(0, 100, 100)
-
-y = -x**2
-
-plt.plot(x,y,"r-")
-plt.show()
--- otros/lectorQR.py
+++
@@ -1,8 +0,0 @@
-from pyzbar.pyzbar import decode
-from PIL import Image
-
-img = Image.open('E:\codesQR\myqr.png')
-
-result = decode(img)
-
-print(result)
