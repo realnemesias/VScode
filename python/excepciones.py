1b61d35496ed54a58ba07d9ac51bf083744374ff
Julio Ramirez <51478458+realnemesias@users.noreply.github.com>
2023-07-17 17:58:13 -0600

    agregando archivos python

--- python/excepciones.py
+++ python/excepciones.py
@@ -5,13 +5,19 @@ Created on Fri Jun 23 23:32:51 2023
 @author: realn
 """
 
-num1 = int(input("ingresa un numero"))
-num2 = int(input("ingresa otro numero"))
+num1 = int(input("ingresa un numero: "))
+num2 = int(input("ingresa otro numero: "))
 
 try:
     resultado = num1/num2
-    print(resultado)
-    
+
+    if num1%num2 == 0:
+      print("es multiplo")
+    elif num2 == 0:
+       print('indefinido')
+    else:
+      print("no es multiplo")
+
 except:
     print("sistem alert")
     
@@ -19,4 +25,5 @@ else:
     print("casi")
     
 finally:
+    print(resultado)
     print("gracias")
