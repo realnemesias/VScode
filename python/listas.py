71793b735d2124c6ff5d23f553bc215b83cea585
Julio Ramirez <51478458+realnemesias@users.noreply.github.com>
2023-07-17 18:00:08 -0600

    terminando listas

---
+++ python/listas.py
@@ -0,0 +1,26 @@
+# tupla () no se puede modificar
+# lista [] si se puede modificar
+# set {}
+# string " "
+# diccionarios {"a":"b"}
+
+
+tupla = tuple() #("1234")
+lista = list() #"132"
+conjunto = set() #
+linea = str() #con o sin ""
+diccionario = dict(calle = "san ricardo", colonia = "santa ursula" )
+
+# print(lista)
+
+a = () #tuple
+b = [] #list
+c = {} #dict
+d = "" #str
+e = {'1','2'} #set
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
---
+++ python/loops.py
@@ -0,0 +1,7 @@
+### while
+
+my_condition = 0
+
+while my_condition < 10:
+    print(my_condition)
+    my_condition += 1
